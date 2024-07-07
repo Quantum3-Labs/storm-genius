e4cf13c43f235b44bc713165c5b60aa97677d0e4
FlyingWalnuts <stonekettle@proton.me>
2024-07-07 10:09:08 +0000

    update explanations

--- utils/api.py
+++ utils/api.py
@@ -80,27 +80,19 @@ def get_emotion_labels(ids):
     return response.json().get("body", [])
 
 def get_gpt_explanation(social_score):
-    # openai.api_key = OPENAI_API_KEY
-    # prompt = f"The social score of the user is {social_score}. Explain this metric in two lines."
-    # response = openai.Completion.create(
-    #     engine="text-davinci-003",
-    #     prompt=prompt,
-    #     max_tokens=50
-    # )
-    # explanation = response.choices[0].text.strip()
-    # return explanation
+    url = "https://api.corcel.io/v1/text/cortext/chat"
 
-    url = "https://api.corcel.io/v1/chat/completions"
-    
     payload = {
-        "model": "llama-3",
-        "temperature": 0.1,
-        "max_tokens": 500,
+        "model": "cortext-ultra",
+        "stream": False,
+        "top_p": 1,
+        "temperature": 0.0001,
+        "max_tokens": 4096,
         "messages": [
             {
                 "role": "user",
                 "content": f"The social score of the user is {social_score}. Explain this metric in two lines."
-            }
+            },
         ]
     }
     headers = {
@@ -108,10 +100,10 @@ def get_gpt_explanation(social_score):
         "content-type": "application/json",
         "Authorization": "e83c689e-e526-4738-996a-f62b5bd5aade"
     }
-    
+
     response = requests.post(url, json=payload, headers=headers)
-    
-    return response.text
+
+    return response.json()[0]['choices'][0]['delta']['content']
 
 from web3 import Web3
 def send_message_to_contract(loan_id):
